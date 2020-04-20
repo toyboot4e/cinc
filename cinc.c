ca992a9544bce5d19b79e8501c5fb15208f07c29
toyboot4e <toyboot4e@gmail.com>
2020-04-20 15:11:11 +0900

    add addition & substraction

--- cinc.c
+++ cinc.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_header() {
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Give me two arguments\n");
     return 1;
   }
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
+  char *p = argv[1];
+
+  print_header();
+
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "Unexpected char: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
