963764a192ec50061e7f13cabaaa8006fcec279c
toyboot4e <toyboot4e@gmail.com>
2020-04-27 11:10:40 +0900

    add parser

---
+++ parser.h
@@ -0,0 +1,33 @@
+//! † The parser †
+
+#ifndef CINC_PARSER_H
+#define CINC_PARSER_H
+
+#include "tokenizer.h"
+
+typedef enum {
+  ND_NUM,
+
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val; // available only if kind == ND_NUM
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+#endif
